903f7c7a746c32c3a2b9cea00599fc69b1576878
Martin Grahovski <99210445+Martinessk0@users.noreply.github.com>
2022-11-08 12:24:06 +0200

    Burger And Drink Entities Added

---
+++ BurgerManiaApp/BurgerManiaApp/Data/Entities/Menu/FrenchFries.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BurgerManiaApp.Data.Entities.Menu
+{
+    public class FrenchFries
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(25)]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [StringLength(200)]
+        public string Description { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string ImageUrl { get; set; }
+    }
+}
