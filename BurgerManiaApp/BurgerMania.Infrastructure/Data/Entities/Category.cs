462cafb34243135df361e9c2da64580072eb831c
Martin Grahovski <martingrahovski2004@gmail.com>
2022-11-23 11:51:23 +0200

    Product and Category Class Added

---
+++ BurgerManiaApp/BurgerMania.Infrastructure/Data/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BurgerManiaApp.Infrastructure.Data.Entities
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ BurgerManiaApp/BurgerMania.Infrastructure/Data/Entities/Product.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurgerManiaApp.Infrastructure.Data.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [StringLength(200)]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [Column(TypeName = "money")]
+        [Precision(18, 2)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
+    }
+}
