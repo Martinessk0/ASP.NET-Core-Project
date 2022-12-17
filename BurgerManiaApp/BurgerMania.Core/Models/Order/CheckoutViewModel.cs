8360808e0adb6f318d7c50f2b58c5d231718aec9
Martin Grahovski <martingrahovski2004@gmail.com>
2022-12-17 23:29:08 +0200

    Added validation to the Checkout menu

--- BurgerManiaApp/BurgerMania.Core/Models/Order/CheckoutViewModel.cs
+++ BurgerManiaApp/BurgerMania.Core/Models/Order/CheckoutViewModel.cs
@@ -8,30 +8,36 @@ namespace BurgerManiaApp.Core.Models.Order
     {
         public string? OrderNumber { get; set; }
 
+        [Required]
         [StringLength(ZipCodeMaxLength, MinimumLength = ZipCodeMinLength)]
-        public string? PostalCode { get; set; }
-        [StringLength(StreetAddressMaxLength,MinimumLength = StreetAddressMinLength)]
-        public string? StreetAddress { get; set; }
+        public string PostalCode { get; set; }  = null!;
 
+        [Required]
+        [StringLength(StreetAddressMaxLength,MinimumLength = StreetAddressMinLength)]
+        public string StreetAddress { get; set; } = null!;
+
+        [Required]
         [StringLength(AddressNameMaxLength, MinimumLength = AddressNameMinLength)]
-        public string? AddressName { get; set; }
+        public string AddressName { get; set; } = null!;
 
         [StringLength(CityMaxLength, MinimumLength = CityMinLength)]
-        public string? City { get; set; }
+        public string City { get; set; } = null!;
 
+        [Required]
         [EmailAddress]
-        public string? EmailAddress { get; set; }
+        public string EmailAddress { get; set; } = null!;
 
+        [Required]
         [StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength)]
-        public string? PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; } = null!;
 
         [Required]
         [StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength)]
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; } = null!;
 
         [Required]
         [StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength)]
-        public string? LastName { get; set; }
+        public string LastName { get; set; } = null!;
 
 
         public ShoppingCartDto? UserCart { get; set; }
